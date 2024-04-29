1714b46834189fef81301f2b1c9a78a573796af1
Ahmed Osama <32176872+ahmedosama07@users.noreply.github.com>
2024-04-29 06:26:07 +0300

    fix: fixed point

--- threads/fixed-point.h
+++ threads/fixed-point.h
@@ -4,24 +4,15 @@
 #include <stdint.h>
 
 // Number of bits representing the fractional part
-#define q 14
+#define Q 14
 
 // Conversion factor from integer to fixed-point
-#define f (1<<q)
-
-// Masks for separating integer and fractional parts
-#define PMASK (-1)<<q
-#define QMASK ~(PMASK)
-
-// Macro to convert integer to fixed-point
-#define to_real(n) int_to_real(f*n);
+#define F (1 << Q)
 
 // Structure to represent fixed-point numbers
 typedef struct
 {
-    int32_t real;       // Fixed-point representation
-    int32_t integer;    // Integer part
-    int32_t decimal;    // Fractional part
+    int32_t value;       // Fixed-point representation
 } real_t;
 
 /**
@@ -29,14 +20,29 @@ typedef struct
  * @param n The integer to be converted.
  * @return The fixed-point representation of the integer.
  */
-real_t int_to_real(int32_t n);
+static inline real_t to_real(int32_t n) {
+    return (real_t){n * F};
+}
 
 /**
  * Converts a fixed-point number to an integer.
  * @param x The fixed-point number to be converted.
  * @return The integer representation of the fixed-point number.
  */
-int32_t to_int(real_t x);
+static inline int32_t to_int(real_t x) {
+    return x.value / F;
+}
+
+/**
+ * Rounds a fixed-point number to an integer.
+ * @param x The fixed-point number to be converted.
+ * @return The integer representation of the fixed-point number.
+ */
+static inline int32_t round_real(real_t x) {
+    if (x.value >= 0)
+        return (x.value + (F / 2)) /F;
+    return (x.value - (F / 2)) /F;
+}
 
 /**
  * Adds two fixed-point numbers.
@@ -44,7 +50,9 @@ int32_t to_int(real_t x);
  * @param y The second fixed-point number.
  * @return The result of the addition.
  */
-real_t add_real(real_t x, real_t y);
+static inline real_t add_real(real_t x, real_t y) {
+    return (real_t){x.value + y.value};
+}
 
 /**
  * Subtracts one fixed-point number from another.
@@ -52,7 +60,9 @@ real_t add_real(real_t x, real_t y);
  * @param y The fixed-point number to subtract.
  * @return The result of the subtraction.
  */
-real_t subtract_real(real_t x, real_t y);
+static inline real_t subtract_real(real_t x, real_t y) {
+    return (real_t){x.value - y.value};
+}
 
 /**
  * Divides one fixed-point number by another.
@@ -60,7 +70,9 @@ real_t subtract_real(real_t x, real_t y);
  * @param y The denominator.
  * @return The result of the division.
  */
-real_t multiply_real(real_t x, real_t y);
+static inline real_t divide_real(real_t x, real_t y) {
+    return (real_t){((int64_t)x.value) * F / y.value};
+}
 
 /**
  * Multiplies two fixed-point numbers.
@@ -68,7 +80,9 @@ real_t multiply_real(real_t x, real_t y);
  * @param y The second fixed-point number.
  * @return The result of the multiplication.
  */
-real_t divide_real(real_t x, real_t y);
+static inline real_t multiply_real(real_t x, real_t y) {
+    return (real_t){((int64_t)x.value) * y.value / F};
+}
 
 /**
  * Adds an integer to a fixed-point number.
@@ -76,7 +90,9 @@ real_t divide_real(real_t x, real_t y);
  * @param n The integer to add.
  * @return The result of the addition.
  */
-real_t add_int(real_t x, int32_t n);
+static inline real_t add_int(real_t x, int32_t n) {
+    return (real_t){x.value + n * F};
+}
 
 /**
  * Subtracts an integer from a fixed-point number.
@@ -84,7 +100,9 @@ real_t add_int(real_t x, int32_t n);
  * @param n The integer to subtract.
  * @return The result of the subtraction.
  */
-real_t subtract_int(real_t x, int32_t n);
+static inline real_t subtract_int(real_t x, int32_t n) {
+    return (real_t){x.value - n * F};
+}
 
 /**
  * Divides a fixed-point number by an integer.
@@ -92,7 +110,9 @@ real_t subtract_int(real_t x, int32_t n);
  * @param n The integer denominator.
  * @return The result of the division.
  */
-real_t multiply_int(real_t x, int32_t n);
+static inline real_t multiply_int(real_t x, int32_t n) {
+    return (real_t){x.value * n};
+}
 
 /**
  * Multiplies a fixed-point number by an integer.
@@ -100,7 +120,9 @@ real_t multiply_int(real_t x, int32_t n);
  * @param n The integer multiplier.
  * @return The result of the multiplication.
  */
-real_t divide_int(real_t x, int32_t n);
+static inline real_t divide_int(real_t x, int32_t n) {
+    return (real_t){x.value / n};
+}
 
 
 #endif /* threads/fixed-point.h */
