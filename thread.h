84fed9c322f62ad3071a1d795ad29a3f1752f83d
ziadelbouriny <100305058+ziadelbouriny@users.noreply.github.com>
2024-05-05 04:13:40 +0300

    primitive begining USERPROG
    Just modifying,
    userprog/process.c -> process_execute(), start_process()
    userprog/process.h -> make new structure for PCB
    threads/thread.h -> forming new list for childs

--- thread.h
+++ thread.h
@@ -106,6 +106,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    struct list child_list;
 #endif
 
     /* Owned by thread.c. */
