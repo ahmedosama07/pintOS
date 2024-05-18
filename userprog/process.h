d2b2caf4c99da603f01081fa5970df392649be2f
Ahmed Osama <32176872+ahmedosama07@users.noreply.github.com>
2024-05-18 09:00:29 +0300

    revert userprog

--- userprog/process.h
+++ userprog/process.h
@@ -2,35 +2,10 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
-#include "threads/synch.h"
-
-typedef int pid_t;
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
-struct process_control_block
-{
-    pid_t pid;
-    struct thread *parent;
-    struct list_elem elem;
-    char *commands;
-
-    bool waiting;
-    bool exited;
-    bool orphan;
-    int exitcode;
-
-    struct semaphore sema_initialization;  
-    struct semaphore sema_wait;
-};
-
-struct file_descriptor
-{
-  int id;
-  struct list_elem elem;
-  struct file* file;
-};
 #endif /* userprog/process.h */
